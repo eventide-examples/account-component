13737bbbc3e02fd6aa071f2eb1c67cd0994ed99a
ntl <nathanladd+github@gmail.com>
2017-04-20 16:46:28 -0500

    Initial commit

---
+++ test/test_init.rb
@@ -0,0 +1,15 @@
+ENV['CONSOLE_DEVICE'] ||= 'stdout'
+ENV['LOG_LEVEL'] ||= '_min'
+
+puts RUBY_DESCRIPTION
+
+require_relative '../init.rb'
+
+require 'test_bench'; TestBench.activate
+
+require 'pp'
+
+require 'account_component/controls'
+
+module AccountComponent; end
+include AccountComponent
