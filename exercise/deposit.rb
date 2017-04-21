1ba1bd119659c3104cbece9f1257302eb540af5e
ntl <nathanladd+github@gmail.com>
2017-04-21 13:05:43 -0500

    [STEP] Random IDs are used by exercises and tests

--- exercise/deposit.rb
+++ exercise/deposit.rb
@@ -1,11 +1,13 @@
 require_relative './exercise_init'
 
+account_id = Identifier::UUID::Random.get
+
 deposit = Messages::Commands::Deposit.new
-deposit.account_id = '123'
+deposit.account_id = account_id
 deposit.amount = 11
 deposit.time = '2000-01-01T11:11:11.000Z'
 
-command_stream_name = 'account:command-123'
+command_stream_name = "account:command-#{account_id}"
 
 Messaging::Postgres::Write.(deposit, command_stream_name)
 
