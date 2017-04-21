71560afa3d26625579ba90b857cc235e5c589de4
ntl <nathanladd+github@gmail.com>
2017-04-21 15:53:34 -0500

    [STEP] Controls

--- lib/account_component/controls.rb
+++ lib/account_component/controls.rb
@@ -1,2 +1,14 @@
 require 'clock/controls'
 require 'identifier/uuid/controls'
+
+require 'account_component/controls/id'
+require 'account_component/controls/time'
+require 'account_component/controls/money'
+
+require 'account_component/controls/account'
+
+require 'account_component/controls/commands/deposit'
+require 'account_component/controls/commands/withdraw'
+
+require 'account_component/controls/events/deposited'
+require 'account_component/controls/events/withdrawn'
