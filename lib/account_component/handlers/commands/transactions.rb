0765cf2bde2bdc5b556c04b35698e77d6b24f12a
ntl <nathanladd+github@gmail.com>
2017-04-24 12:14:15 -0500

    [COMPLETED] Withdrawal reservation

--- lib/account_component/handlers/commands/transactions.rb
+++ lib/account_component/handlers/commands/transactions.rb
@@ -44,7 +44,36 @@ module AccountComponent
         end
 
         handle Withdraw do |withdraw|
-          # TODO Move implementation from commands handler
+          account_id = withdraw.account_id
+
+          account, version = store.fetch(account_id, include: :version)
+
+          sequence = withdraw.metadata.global_position
+
+          if account.processed?(sequence)
+            logger.info(tag: :ignored) { "Command ignored (Command: #{withdraw.message_type}, Account ID: #{account_id}, Account Sequence: #{account.sequence}, Withdrawal Sequence: #{sequence})" }
+            return
+          end
+
+          time = clock.iso8601
+
+          stream_name = stream_name(account_id)
+
+          unless account.sufficient_funds?(withdraw.amount)
+            withdrawal_rejected = WithdrawalRejected.follow(withdraw)
+            withdrawal_rejected.time = time
+            withdrawal_rejected.sequence = sequence
+
+            write.(withdrawal_rejected, stream_name, expected_version: version)
+
+            return
+          end
+
+          withdrawn = Withdrawn.follow(withdraw)
+          withdrawn.processed_time = time
+          withdrawn.sequence = sequence
+
+          write.(withdrawn, stream_name, expected_version: version)
         end
       end
     end
