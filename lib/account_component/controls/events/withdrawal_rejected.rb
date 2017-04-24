c89e2a62e946b0c1b933c78b401c3cd8b6705beb
ntl <nathanladd+github@gmail.com>
2017-04-24 11:27:44 -0500

    [STEP] Idempotent withdrawal

---
+++ lib/account_component/controls/events/withdrawal_rejected.rb
@@ -0,0 +1,19 @@
+module AccountComponent
+  module Controls
+    module Events
+      module WithdrawalRejected
+        def self.example
+          withdrawal_rejected = AccountComponent::Messages::Events::WithdrawalRejected.build
+
+          withdrawal_rejected.account_id = Account.id
+          withdrawal_rejected.amount = Money.example
+          withdrawal_rejected.time = Controls::Time::Effective.example
+
+          withdrawal_rejected.sequence = Sequence.example
+
+          withdrawal_rejected
+        end
+      end
+    end
+  end
+end
