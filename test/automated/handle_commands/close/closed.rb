094377706f609a6fdf3fa4e2e108cb1bece51ed9
Nathan Ladd <nathanladd@gmail.com>
2020-08-12 10:13:37 -0500

    Close command handler tests use handler fixture

--- test/automated/handle_commands/close/closed.rb
+++ test/automated/handle_commands/close/closed.rb
@@ -4,49 +4,33 @@ context "Handle Commands" do
   context "Close" do
     context "Closed" do
       handler = Handlers::Commands.new
-
-      processed_time = Controls::Time::Processed::Raw.example
-
-      handler.clock.now = processed_time
-
       close = Controls::Commands::Close.example
 
-      account_id = close.account_id or fail
-      effective_time = close.time or fail
+      account = Controls::Account::Open.example
+      account_version = Controls::Version.example
 
-      handler.(close)
+      clock_time = Controls::Time::Processed::Raw.example
 
-      writer = handler.write
-
-      closed = writer.one_message do |event|
-        event.instance_of?(Messages::Events::Closed)
-      end
-
-      test "Closed Event is Written" do
-        refute(closed.nil?)
-      end
-
-      test "Written to the account stream" do
-        written_to_stream = writer.written?(closed) do |stream_name|
-          stream_name == "account-#{account_id}"
+      fixture(
+        Messaging::Fixtures::Handler,
+        handler,
+        close,
+        account,
+        account_version,
+        clock_time: clock_time
+      ) do |handler|
+        closed = handler.assert_write(Messages::Events::Closed) do |write|
+          write.assert_stream_name("account-#{account.id}")
+          write.assert_expected_version(account_version)
         end
 
-        assert(written_to_stream)
-      end
+        handler.assert_written_message(closed) do |closed|
+          closed.assert_attributes_copied([
+            :account_id,
+            :time
+          ])
 
-      context "Attributes" do
-        test "account_id" do
-          assert(closed.account_id == account_id)
-        end
-
-        test "time" do
-          assert(closed.time == effective_time)
-        end
-
-        test "processed_time" do
-          processed_time_iso8601 = Clock::UTC.iso8601(processed_time)
-
-          assert(closed.processed_time == processed_time_iso8601)
+          closed.assert_attribute_value(:processed_time, Clock.iso8601(clock_time))
         end
       end
     end
--- test/automated/handle_commands/close/ignored.rb
+++ test/automated/handle_commands/close/ignored.rb
@@ -4,23 +4,17 @@ context "Handle Commands" do
   context "Close" do
     context "Ignored" do
       handler = Handlers::Commands.new
-
       close = Controls::Commands::Close.example
 
       account = Controls::Account::Closed.example
 
-      handler.store.add(account.id, account)
-
-      handler.(close)
-
-      writer = handler.write
-
-      closed = writer.one_message do |event|
-        event.instance_of?(Messages::Events::Closed)
-      end
-
-      test "Closed Event is not Written" do
-        assert(closed.nil?)
+      fixture(
+        Messaging::Fixtures::Handler,
+        handler,
+        close,
+        account
+      ) do |handler|
+        handler.refute_write
       end
     end
   end
