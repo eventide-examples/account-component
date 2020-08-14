956ec665c9ece957e99e10c719e818b02d27b0b7
Nathan Ladd <nathanladd@gmail.com>
2020-08-14 16:50:22 -0500

    Opened projection test uses projection fixture

--- test/automated/projection/opened.rb
+++ test/automated/projection/opened.rb
@@ -6,25 +6,20 @@ context "Projection" do
 
     assert(account.opened_time.nil?)
 
+    projection = Projection.build(account)
     opened = Controls::Events::Opened.example
 
-    customer_id = opened.customer_id or fail
-    account_id = opened.account_id or fail
+    fixture(
+      EntityProjection::Fixtures::Projection,
+      projection,
+      opened
+    ) do |fixture|
+      fixture.assert_attributes_copied([
+        { :account_id => :id },
+        :customer_id
+      ])
 
-    Projection.(account, opened)
-
-    test "ID is set" do
-      assert account.id == account_id
-    end
-
-    test "Customer ID is set" do
-      assert(account.customer_id == customer_id)
-    end
-
-    test "Open time is converted and copied" do
-      opened_time = Time.parse(opened.time)
-
-      assert(account.opened_time == opened_time)
+      fixture.assert_transformed_and_copied(:time => :opened_time) { |v| Time.parse(v) }
     end
   end
 end
